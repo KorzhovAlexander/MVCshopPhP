1d32351ab6bcde6c6b4706cdba6166537ec391d1
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-13 02:26:03 +0300

    footer add

--- view/temp/footer.php
+++ view/temp/footer.php
@@ -1,3 +1,39 @@
+<footer class="page-footer green-c">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Company Bio</h5>
+                <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+            </div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Settings</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">Link 1</a></li>
+                    <li><a class="white-text" href="#!">Link 2</a></li>
+                    <li><a class="white-text" href="#!">Link 3</a></li>
+                    <li><a class="white-text" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Connect</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">Link 1</a></li>
+                    <li><a class="white-text" href="#!">Link 2</a></li>
+                    <li><a class="white-text" href="#!">Link 3</a></li>
+                    <li><a class="white-text" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            Made by <a class="green-text text-lighten-3" href="#!">KORZHOV Alexander</a>
+        </div>
+    </div>
+</footer>
+
 <!--JavaScript at end of body for optimized loading-->
 <script type="text/javascript" src="/view/resources/js/materialize.js"></script>
 
