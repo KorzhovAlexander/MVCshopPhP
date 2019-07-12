2da9bbbd2fb77b58e9c2720271e9935174a2c3a4
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-12 18:49:29 +0300

    registration page rus

--- view/auth/registration.php
+++ view/auth/registration.php
@@ -6,7 +6,7 @@
         <div class="card login-form">
             <div class="card-content">
                 <!-- title -->
-                <div class="center-align border title-border m1">Create your account.</div>
+                <div class="center-align border title-border m1">Создать учетную запись.</div>
 
                 <!-- card body -->
                 <div class="card-body">
@@ -18,7 +18,7 @@
                             <div class="input-field col s12">
                                 <i class="material-icons prefix">account_box</i>
                                 <input id="name" type="text" class="validate">
-                                <label for="name">Name</label>
+                                <label for="name">Имя</label>
                             </div>
 
                             <div class="input-field col s12">
@@ -30,17 +30,26 @@
                             <div class="input-field col s12">
                                 <i class="material-icons prefix">lock_outline</i>
                                 <input id="password" type="password" class="validate">
-                                <label for="password">Password</label>
+                                <label for="password">Пароль</label>
                             </div>
 
                             <div class="input-field col s12">
                                 <i class="material-icons prefix">lock</i>
                                 <input id="confirm-password" type="password" class="validate">
-                                <label for="confirm-password">Confirm Password</label>
+                                <label for="confirm-password">Подтвердите Пароль</label>
                             </div>
 
+                            <div class="col s12 center-align ">
+                                <p>
+                                    <label>
+                                        <input type="checkbox" class="filled-in" name="check">
+                                        <span class="small">Я СОГЛАСЕН <b>на обработку</b> и хранение моих <b>персональных данных</b></span>
+                                    </label>
+                                </p>
+                            </div>
+<!---->
                             <div class="input-field col s12">
-                                <button class="btn btn-block waves-effect waves-light" type="submit" name="action">Create your account
+                                <button class="btn btn-block waves-effect waves-light" type="submit" name="action">Создать учетную запись
                                 </button>
                             </div>
                         </div>
@@ -49,8 +58,8 @@
 
                     <!-- form footer -->
                     <div class="center-align">
-                        Already have an account?
-                        <a href="/login">Sign in</a></div>
+                        Уже есть аккаунт?
+                        <a href="/login">Войти</a></div>
                     <!-- End form footer -->
 
                 </div>
