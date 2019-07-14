f718d24d9fe071e4e395b488ebe16f9e50e3b50e
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-14 17:46:28 +0300

    code review

--- controller/productsController.php
+++ controller/productsController.php
@@ -3,19 +3,31 @@
 
 class productsController
 {
+    private static function getListCategory()
+    {
+        return categoryModel::getCategoriesList();
+    }
+
+    /* Для работы пагинации */
+    private static function startPagination($idCategory,$page)
+    {
+        // Общее количетсво товаров (необходимо для постраничной навигации)
+        $total = productsModel::getTotalProductsInCategory($idCategory);
+        // Создаем объект Pagination - постраничная навигация
+        return new Pagination($total, $page, productsModel::SHOW_DEFAULT, 'page-');
+    }
+
+
     public function listAction($page=1)
     {
         // Список категорий для левого меню
-        $categorylist=categoryModel::getCategoriesList();
+        $categorylist= self::getListCategory();
 
         // Список всех товаров (особенно последних)
         $products_array=productsModel::getProducts($page);
 
-        /* */
-        // Общее количетсво товаров (необходимо для постраничной навигации)
-        $total = productsModel::getTotalProductsInCategory(0);
-        // Создаем объект Pagination - постраничная навигация
-        $pagination = new Pagination($total, $page, productsModel::SHOW_DEFAULT, 'page-');
+        $pagination=self::startPagination(0,$page);
+
 
         require_once (ROOT.'/view/products/products.php');
         return true;
@@ -24,16 +36,13 @@ class productsController
     public function listCategoryAction($idCategory,$page=1)
     {
         // Список категорий для левого меню
-        $categorylist=categoryModel::getCategoriesList();
+        $categorylist= self::getListCategory();
 
         // Список товаров в категории
         $products_array=productsModel::getProductsListByCategory($idCategory,$page);
 
-        /* */
-        // Общее количетсво товаров (необходимо для постраничной навигации)
-        $total = productsModel::getTotalProductsInCategory($idCategory);
-        // Создаем объект Pagination - постраничная навигация
-        $pagination = new Pagination($total, $page, productsModel::SHOW_DEFAULT, 'page-');
+        $pagination=self::startPagination($idCategory,$page);
+
         require_once (ROOT.'/view/products/products.php');
         return true;
     }
