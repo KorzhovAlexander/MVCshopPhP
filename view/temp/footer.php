b4cbfbf70161cd4670ab947936f36e9beb60ae5a
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-15 21:27:50 +0300

    footer redesign

--- view/temp/footer.php
+++ view/temp/footer.php
@@ -2,34 +2,35 @@
     <div class="container">
         <div class="row">
             <div class="col l6 s12">
-                <h5 class="white-text">Company Bio</h5>
+                <h5 class="white-text">Информация о магазине</h5>
                 <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
 
 
             </div>
             <div class="col l3 s12">
-                <h5 class="white-text">Settings</h5>
+                <h5 class="white-text">Contact us</h5>
                 <ul>
-                    <li><a class="white-text" href="#!">Link 1</a></li>
-                    <li><a class="white-text" href="#!">Link 2</a></li>
-                    <li><a class="white-text" href="#!">Link 3</a></li>
-                    <li><a class="white-text" href="#!">Link 4</a></li>
+                    <li><a class="white-text" href="#!"><i class="fa fa-vk" aria-hidden="true"></i> Вконтакте</a></li>
+                    <li><a class="white-text" href="#!"><i class="fa fa-telegram" aria-hidden="true"></i> Telegram</a></li>
+                    <li><a class="white-text" href="#!"><i class="fa fa-odnoklassniki" aria-hidden="true"></i> Одноклассники</a></li>
+                    <li><a class="white-text" href="#!"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a></li>
                 </ul>
             </div>
             <div class="col l3 s12">
-                <h5 class="white-text">Connect</h5>
+                <h5 class="white-text">Код проекта</h5>
                 <ul>
-                    <li><a class="white-text" href="#!">Link 1</a></li>
-                    <li><a class="white-text" href="#!">Link 2</a></li>
-                    <li><a class="white-text" href="#!">Link 3</a></li>
-                    <li><a class="white-text" href="#!">Link 4</a></li>
+                    <li>
+                        <a class="white-text" href="https://github.com/KorzhovAlexander/MVCshopPhP">
+                            <i class="fa fa-github" aria-hidden="true"></i> GitHub
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
     </div>
     <div class="footer-copyright">
         <div class="container">
-            Made by <a class="green-text text-lighten-3" href="#!">KORZHOV Alexander</a>
+            Made by <a class="green-text text-lighten-3" href="https://vk.com/solifax">KORZHOV Alexander</a>
         </div>
     </div>
 </footer>
